9f404d2accb5283a4a346415cf577beec5f00d51
Cristian Soare <cristian.soare12@outlook.com>
2024-10-22 13:42:00 +0200

    feat: First commit with project description, gitignore and TOML

---
+++ src/controller/__main__.py
@@ -0,0 +1,7 @@
+class Main():
+  @staticmethod
+  def main():
+    print("Hello from Controller!")
+
+if __name__ == "__main__":
+    Main.main()
---
+++ src/generator/__main__.py
@@ -0,0 +1,7 @@
+class Main():
+  @staticmethod
+  def main():
+    print("Hello from Generator!")
+
+if __name__ == "__main__":
+    Main.main()
